cf309495ce9add8770cbf0ffc32c3c8a44903a02
Eoin Murphy <pixelctrl@me.com>
2017-02-08 19:08:00 +0000

    Test will fail?

--- test/meaningOfLife.test.js
+++ test/meaningOfLife.test.js
@@ -3,5 +3,5 @@ const test = require('ava')
 const meaningOfLife = require('../lib/meaningOfLife')
 
 test('Real meaning of life', (t) => {
-  t.is(meaningOfLife(), 42)
+  t.id(meaningOfLife(), 42)
 })
